71560cf0f6ef589947c2550c28cfc82a359f63fa
cclauss <cclauss@bluewin.ch>
2017-08-31 11:34:42 +0200

    print() function

--- build/cleanup.py
+++ build/cleanup.py
@@ -12,11 +12,11 @@ if fix_files_exist():
     # deleting old ttfs
     for file in os.listdir('.'):
         if file.endswith('ttf'):
-            print 'deleting %s' % os.path.join('.', file)
+            print('deleting %s' % os.path.join('.', file))
             os.remove(os.path.join('.', file))
 
 # Rename fixed fonts to ttfs
 for font in os.listdir('.'):
     font = os.path.join('.', font)
-    print 'renaming %s' % font
+    print('renaming %s' % font)
     os.rename(font, re.sub(r'.fix|.renamed', '', font))
